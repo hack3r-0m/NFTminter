6c395ca35e461ccc3a1f697758eff7fbd7e07a55
hack3r-0m <54898623+hack3r-0m@users.noreply.github.com>
2021-04-05 21:20:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
     pinata_api_key: process.env.pinata_api_key,
-    pinata_secret_api_key: process.env.pinata_secret_api_key
+    pinata_secret_api_key: process.env.pinata_secret_api_key,
+    biconomy_api_key: process.env.biconomy_api_key
   },
 }
