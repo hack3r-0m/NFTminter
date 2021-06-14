684f34e2620e564dd79d8429685334da23372d43
Aman <archanaamanraj@gmail.com>
2021-06-14 21:45:47 +0530

    covalent api key change added

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     base_url: 'http://localhost:8080',
     pinata_api_key: process.env.pinata_api_key,
     pinata_secret_api_key: process.env.pinata_secret_api_key,
-    biconomy_api_key: process.env.biconomy_api_key
+    biconomy_api_key: process.env.biconomy_api_key,
+    covalent_key: process.env.covalent_key
   },
 }
